d4c3bd1868383307d2766b5dffcd4bafbea7cf56
Nabeel Ashfaq <nabeelashfaq@192.168.1.11>
2024-02-21 02:17:38 +0500

    Created webpack app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// import App from './app'
+import exampleImage from './assets/img-1.png';
+import './app.css'
+
+// You can use exampleImage as the path to the image file
+console.log(exampleImage);
+
+
+console.log("entry point");
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, 
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader', 
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist")
+    }
+  },
+};
